bde3f41de1ac706bccec04ee126118ff028fa75d
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-13 16:39:16 +0200

    add structure

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,8 @@
-// Ask km numbers
-let kmNumber = parseInt(prompt("How many km will you have to travel?"));
-console.log(kmNumber);
-// Ask age
-let age = parseInt(prompt("How old are you?"));
-console.log(age);
+const passengerName = document.querySelector("#passengerName");
+let kmNumber = document.querySelector("#journeyDistance");
+let age = document.querySelector("#passengerAge");
+let containerFinal = document.querySelector(".final");
+
 // Multiply Km numbers * ticket price
 let kmPrice = kmNumber * 0.21;
 console.log("Your ticket price is:" + " " + kmPrice.toFixed(2) + "€");
@@ -14,15 +13,6 @@ let minorDiscount = (kmPrice * 20) / 100;
 // Over 65 discount
 let overDiscount = (kmPrice * 40) / 100;
 
-// Refresh when user write a wrong value
-if (isNaN(kmNumber)) {
-  alert("You entered a wrong value, refresh the page and try again");
-}
-
-if (isNaN(age)) {
-  alert("You entered a wrong value, refresh the page and try again");
-}
-
 // Discount application
 if (age < 18) {
   kmPrice = kmPrice - minorDiscount.toFixed(2);
@@ -32,5 +22,15 @@ if (age < 18) {
   kmPrice.toFixed(2);
 }
 
-// Stamp in html
-document.getElementById("price").innerHTML = kmPrice.toFixed(2);
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  containerFinal.innerHTML = `${kmPrice.value}`;
+  console.log(
+    "Passenger name: " +
+      passengerName.value +
+      ", Journey distance: " +
+      journeyDistance.value +
+      ", Passenger age: " +
+      passengerAge.value
+  );
+});
